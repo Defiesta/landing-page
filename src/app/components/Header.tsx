1e140294b733e704b05e3523d2a69eaae38ed702
mehdi-defiesta <mberiane@defiesta.net>
2026-01-30 10:34:42 +0100

    X

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -36,6 +36,11 @@ const SOCIAL_ICONS = {
       <path d="M13.54 12a6.8 6.8 0 01-6.77 6.82A6.8 6.8 0 010 12a6.8 6.8 0 016.77-6.82A6.8 6.8 0 0113.54 12zM20.96 12c0 3.54-1.51 6.42-3.38 6.42-1.87 0-3.39-2.88-3.39-6.42s1.52-6.42 3.39-6.42 3.38 2.88 3.38 6.42M24 12c0 3.17-.53 5.75-1.19 5.75-.66 0-1.19-2.58-1.19-5.75s.53-5.75 1.19-5.75C23.47 6.25 24 8.83 24 12z"/>
     </svg>
   ),
+  x: (
+    <svg className="w-4 h-4" fill="currentColor" viewBox="0 0 24 24">
+      <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"/>
+    </svg>
+  ),
 };
 
 // Company sections data
@@ -51,6 +56,7 @@ const COMPANY_SECTIONS: CompanySection[] = [
     heading: "Socials",
     links: [
       { title: "GitHub", description: "View our code", href: "https://github.com/Defiesta", icon: SOCIAL_ICONS.github },
+      { title: "X", description: "Follow us", href: "https://x.com/DeFiestaRoom", icon: SOCIAL_ICONS.x },
       { title: "Telegram", description: "Join our community", href: "https://t.me/defiesta", icon: SOCIAL_ICONS.telegram },
       { title: "Discord", description: "Chat with us", href: "https://discord.gg/v3Se8GytFc", icon: SOCIAL_ICONS.discord },
       { title: "Medium", description: "Read our blog", href: "https://medium.com/@mehd1b", icon: SOCIAL_ICONS.medium },
